5141f61666a8120fdfb2fd2ec847b171106fb753
Arham Mahajan <mahajanarham0@gmail.com>
2026-03-16 11:35:45 +0530

    update: exp-5

--- exp-5/main.c
+++ exp-5/main.c
@@ -17,6 +17,14 @@ Matrix  matMul(Matrix m1, Matrix m2){
 	}
 	return result;
 }
+Matrix initMat(Matrix m1, int c){
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			m1.data[i][j] = c;
+		}	
+	}
+}
+
 
 Matrix translation(Matrix x, int tx, int ty){
 	Matrix t = {{ {1, 0, tx}, {0 ,1, ty}, {0, 0, 1} }};
@@ -24,9 +32,23 @@ Matrix translation(Matrix x, int tx, int ty){
 	return result;
 }
 
+Matrix scaling(Matrix x, int sx, int sy){
+	Matrix t = {{ {sx, 0, 0}, {0, sy, 0}, {0, 0, 1} }};
+	Matrix result = matMul(t, x);
+	
+	return result;
+}
+
+Matrix shearing(Matrix x, int shx, int shy){
+	Matrix t = {{ {1, Shx, 0}, {Shy, 1, 0}, {0, 0, 1} }};
+	Matrix result = matMul(t, x);
+	return result; 
+}
+
+
 int main(){
-	Matrix m1 = {{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}};
-	Matrix result = translation(m1, 5, 4);
+	Matrix m1 = {{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}};
+	Matrix result = scaling(m1, 5, 4);
 	for(int i = 0; i < 3; i++){
 		for(int j = 0; j < 3; j++){
 			printf("%d", result.data[i][j]);
