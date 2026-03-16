52c926f69a9e1314ae440965522217c911757a66
Arham Mahajan <mahajanarham0@gmail.com>
2026-03-16 17:54:45 +0530

    update: exp-5

--- exp-5/main.c
+++ exp-5/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <graphics.h>
 #include <math.h>
-
+#define PI 3.14159265358979323846
 typedef struct {
 	int data[3][3];
 } Matrix;
+
+float degRad (float deg){
+	return (deg * PI)/180;
 
 Matrix  matMul(Matrix m1, Matrix m2){
 	Matrix result = {{{0,0,0}, {0,0,0}, {0,0,0}}};
@@ -45,6 +48,34 @@ Matrix shearing(Matrix x, int shx, int shy){
 	return result; 
 }
 
+Matrix reflectionX(Matrix x){
+	Matrix t = {{ {1, 0, 0}, {0, -1, 0}, {0, 0, 1} }};
+	Matrix result = matMul(t, x);
+	return result;
+}
+
+Matrix reflectionY(Matrix x){
+	Matrix t = {{ {-1, 0, 0}, {0, 1, 0}, {0, 0, 1} }};
+	Matrix result = matMul(t, x);
+	return result;
+}
+
+Matrix reflectionO(Matrix x){
+	Matrix t = {{ {-1, 0, 0}, {0, -1, 0}, {0, 0, 1} }};
+	Matrix result = matMul(t, x);
+	return result;
+}
+
+Matrix reflectionYX(Matrix x){
+	Matrix t = {{ {0, 1, 0}, {1, 0, 0}, {0, 0, 1} }};
+	Matrix result = matMul(t, x);
+	return result;
+}
+
+Matrix rotationZ(Matrix x, int alph){
+	int rad = degRad(alph);
+	Matrix r = {{ {cos(rad), -sin(rad), 0}, {sin(rad), cos(rad), 0}, {0, 0, 1} }};
+} 
 
 int main(){
 	Matrix m1 = {{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}};
